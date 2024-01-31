6de26ea80887165aacdd42297f905b3b6d15539d
Lucas Amaral <lucasamaralfaria@gmail.com>
2024-01-31 10:49:18 -0300

    add home,sign-in and up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        pathMatch: "full",
+    },
+    {
+        path: 'sign-in',
+        component: SignInComponent,
+        pathMatch: "full",
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent,
+        pathMatch: "full",
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        pathMatch: "full",
+    },
+];
