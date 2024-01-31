032526d31a57662ec530eebdb661bf914eaf75a3
Lucas Amaral <lucasamaralfaria@gmail.com>
2024-01-31 12:49:39 -0300

    add login feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { TaskBaseModule } from './task-base/task-base.module';
 import { ListBaseModule } from './list-base/list-base.module';
 import { MenuModule } from './menu/menu.module';
 import { TaskFormModule } from './task-form/task-form.module';
+import { FormsModule } from '@angular/forms';
+import { LoginUserModule } from './login-user/login-user.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,TaskBaseModule,ListBaseModule,MenuModule,TaskFormModule, RouterModule],
+  imports: [CommonModule, FormsModule,RouterOutlet,TaskBaseModule,ListBaseModule,MenuModule,TaskFormModule, LoginUserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
